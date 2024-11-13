edaa718ee7c61e3bbd88e7bb318590ebc6a1d4af
Dale-Heron <heron_dale@yahoo.co.uk>
2024-11-13 12:16:51 +0000

    rearraneg pages and components

---
+++ Components/Models/Player.cs
@@ -0,0 +1,29 @@
+using System.Text.Json.Serialization;
+
+public class Player
+{
+    [JsonPropertyName("first_name")]
+    public required string FirstName { get; set; }
+
+    [JsonPropertyName("second_name")]
+    public required string SecondName { get; set; }
+
+    [JsonPropertyName("total_points")]
+    public required int TotalPoints { get; set; }
+
+    [JsonPropertyName("now_cost")]
+    public required int Price { get; set; }
+
+    [JsonPropertyName("element_type")]
+    public required int Position { get; set; }
+
+    [JsonPropertyName("minutes")]
+    public required int Minutes { get; set; }
+
+    [JsonPropertyName("selected_by_percent")]
+    public required string SelectedByPercent { get; set; }
+
+    public float PriceF => Price*10.0f;
+
+    public float SelectedByPercentF => float.Parse(SelectedByPercent);
+}
