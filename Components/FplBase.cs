a7d520439d8c013d402e2f3f5a2b28e52a07fa0e
Dale-Heron <heron_dale@yahoo.co.uk>
2024-12-04 15:56:04 +0000

    inherited from custom base for fpl page

---
+++ Components/FplBase.cs
@@ -0,0 +1,38 @@
+using FplStatsSsr.Components.Models;
+using FplStatsSsr.Components.Pages.Components;
+using FplStatsSsr.Components.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace FplStatsSsr
+{
+    public class FplBase : ComponentBase
+    {
+        [Inject]
+        private ILogger<FplBase> Logger { get; set; } = default!;
+
+        [Inject]
+        private GetFplDataService FplService { get; set; } = default!;
+
+        protected IEnumerable<Player> TypedPlayers { get; set; } = default!;
+
+        protected PlayerTable? _child; 
+
+        protected override async Task OnInitializedAsync()
+        {
+            Logger.LogInformation("OnInitializedAsync");
+            
+            TypedPlayers = await FplService.GetPlayers();
+            Logger.LogInformation($"Found {TypedPlayers.Count()} players");
+        } 
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            Logger.LogInformation($"OnAfterRenderAsync {firstRender}");
+            
+            if (firstRender && _child!=null)
+            {
+                await _child.SortTableInvokeJsAsync();
+            }
+        } 
+    }
+}
